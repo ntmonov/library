3cf5299bf5817bb053d49a6c27e5352e47d34cab
ntmonov <nmonov84@gmail.com>
2020-04-23 09:25:52 +0300

    added auth module in frontend

--- library/src/app/app.module.ts
+++ library/src/app/app.module.ts
@@ -1,31 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
-import { RegisterComponent } from './components/auth/register/register.component';
 import { MainComponent } from './components/main/main/main.component';
-import { LoginComponent } from './components/auth/login/login.component';
+import { AuthModule } from './components/auth/auth.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    RegisterComponent,
-    MainComponent,
-    LoginComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-  ],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, MainComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, AuthModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ library/src/app/components/auth/auth.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [LoginComponent, RegisterComponent],
+  imports: [CommonModule, ReactiveFormsModule],
+})
+export class AuthModule {}
